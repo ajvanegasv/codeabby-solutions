395a30a6186b5d1a60ad5e7ee758f28fae5aae4e
Alvaro J Vanegas <alvaro@ajvanegasv.dev>
2023-07-06 14:15:53 -0500

    Create 001.f95

---
+++ fortran/001.f95
@@ -0,0 +1,8 @@
+program sum
+    implicit none;
+    integer :: first_number, second_number
+    
+    write(*,*) 'Please enter two digits'
+    read(*,*) first_number, second_number
+    print '(I0)', first_number + second_number
+end program sum
